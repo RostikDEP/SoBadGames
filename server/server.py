e81ebf4bb5d673e3f2fcae3139d2b91e345722f4
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-04-20 02:20:45 +0300

    Created server class

--- server/server.py
+++ server/server.py
@@ -1,9 +1,24 @@
 import socket
+import json
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('109.108.241.14', 1212))
 
-server.listen()
-client, address = server.accept()
-# data = client.recv(1024).decode()
-# print(data)
+class Server:
+    def __init__(self):
+        self.ip = None
+        self.port = None
+        self.server = None
+        self.LoadConfigurations()
+
+    def LoadConfigurations(self):
+        with open("configuration.json", 'r', encoding='utf-8') as config_file:
+            config_json = json.load(config_file)
+            self.ip, self.port = config_json['server_host']['ip'], int(config_json['server_host']['port'])
+
+    def Bind(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.ip, self.port))
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.Bind()
