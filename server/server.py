2895c312a0f2cb78e9d5dcffb0b51149c62e0214
Stadnikov Rostislav <stadnikov2000@gmail.com>
2024-04-20 03:58:20 +0300

    Add threading for clients handling

--- server/server.py
+++ server/server.py
@@ -1,12 +1,13 @@
 import socket
 import json
-
+import threading
 
 class Server:
     def __init__(self):
+        self.server = None
         self.ip = None
         self.port = None
-        self.server = None
+        self.listening = True
         self.LoadConfigurations()
 
     def LoadConfigurations(self):
@@ -17,8 +18,23 @@ class Server:
     def Bind(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.ip, self.port))
+        self.server.listen()
+
+    def StartListeningLoop(self):
+        while self.listening:
+            try:
+                client, address = self.server.accept()
+                thread = threading.Thread(target=self.HandleClient, args=(client, address))
+                thread.start()
+                print(f"Active connections: {threading.active_count() - 1}")
+            except Exception as e:
+                print("Error")
+
+    def HandleClient(self, client, address):
+        print(f"New connection: {address}")
 
 
 if __name__ == "__main__":
     server = Server()
     server.Bind()
+    server.StartListeningLoop()
